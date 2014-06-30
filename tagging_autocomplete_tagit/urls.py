7d54070eb96641ee41f893f72ef0acda3082002d
Christian Kaiser <chri.kais@gmail.com>
2014-06-30 19:34:05 +0200

    Updated to Django 1.6

--- tagging_autocomplete_tagit/urls.py
+++ tagging_autocomplete_tagit/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('tagging_autocomplete_tagit.views',
     url(r'^list$', 'list_tags', name='tagging_autocomplete_tagit-list'),
